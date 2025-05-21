999e95ee634eeb598d7586a7103916855a1e8e9c
NamLeCS <nam.le94568@gmail.com>
2025-05-20 22:55:42 -0700

    Update baseline.py

--- baseline.py
+++ baseline.py
@@ -75,7 +75,7 @@ def get_response(prompt):
         return "Model or tokenizer not loaded."
 
     inputs = tokenizer(prompt, return_tensors="pt").to(device)
-    inputs_id_length = inputs.input_ids.shape[1]
+    inputs_ids_length = inputs.input_ids.shape[1]
     # log timestamp
     start_time = time.time()
     print("Generating response...")
